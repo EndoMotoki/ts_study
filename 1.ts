df7ec4aca674eb3ef4043819d053eac60f26f2ae
EndoMotoki <endomotoki0626@gmail.com>
2025-04-10 23:44:30 +0900

    宣言と型アノテーション

--- 1.ts
+++ 1.ts
@@ -1,2 +1,18 @@
-let word = "hello";
-word = "こんにちは";
+function addOne(n: number) {
+    return n+1;
+}
+
+function concatOne(n: string): string {
+    return n + "One";
+}
+
+const addOne = function (n: number) {
+    return n + 1;
+}
+
+const addOne1 = (n: number) => {
+    return n + 1;
+}
+
+// 関数本体が単体の場合はさらに省略できる
+const addOne2 = (n: number) => n + 1;
