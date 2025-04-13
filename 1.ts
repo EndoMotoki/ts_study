cff9558f7a7b77144ed580510e5a169712ae5999
EndoMotoki <endomotoki0626@gmail.com>
2025-04-13 21:19:12 +0900

    宣言と型アノテーション

--- 1.ts
+++ 1.ts
@@ -1,17 +1,5 @@
-// 値としてのオブジェクト
-const mail1 = {
-    name: "通常はがき",
-    price: 85,
+const hydrogen = {
+    protons: 1,
 };
 
-// 型エイリアスを使ったオブジェクトの型定義
-type MailType = {
-    name: string;
-    price: number;
-};
-
-// 型アノテーションされた変数を宣言してオブジェクトを代入
-const mail2: MailType = {
-    name: "定形郵便物",
-    price: 110,
-};
+const numberOfNeutrons = hydrogen?.neutrons;
